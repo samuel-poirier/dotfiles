a601453bdefdd48955b7f1256eb83fdb6524b94f
sam9291 <sam9291p@gmail.com>
2025-10-19 09:58:39 -0400

    Finished configuring roslyn.nvim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,12 +2,14 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local home_path = vim.env.HOME
+
 vim.g.lazyvim_picker = "telescope"
 
 vim.lsp.config("roslyn", {
   cmd = {
-    "~/.dotnet/dotnet",
-    "~/csharp-ls/content/LanguageServer/linux-x64/Microsoft.CodeAnalysis.LanguageServer.dll",
+    home_path .. "/.dotnet/dotnet",
+    home_path .. "/csharp-ls/content/LanguageServer/linux-x64/Microsoft.CodeAnalysis.LanguageServer.dll",
     "--logLevel=Information",
     "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
     "--stdio", -- Flag does not exist on this version
--- nvim/.config/nvim/lua/plugins/colorsheme.lua
+++ nvim/.config/nvim/lua/plugins/colorsheme.lua
@@ -1,14 +1,11 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
   -- Configure LazyVim to load tokyonight
   {
     "LazyVim/LazyVim",
     "folke/tokyonight.nvim",
     "oxfist/night-owl.nvim",
     opts = {
-      colorscheme = "night-owl",
+      colorscheme = "tokyonight",
     },
   },
 }
