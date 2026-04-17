065772cf48ff70f27e5de025a13d99d7ce8a5837
Samuel Poirier <samuel.poirier@beslogic.com>
2026-04-17 09:12:30 -0400

    Add config backup script

---
+++ omarchy/.config/backup-stow-configs.sh
@@ -0,0 +1,3 @@
+cp ~/.config/hypr/bindings.conf ~/.config/hypr/bindings.conf.$(date +%F).bak
+cp ~/.config/tmux/tmux.conf ~/.config/tmux/tmux.conf.$(date +%F).bak
+cp ~/.bashrc ~/.bashrc.$(date +%F).bak
