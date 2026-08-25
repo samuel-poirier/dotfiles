1afb9ab5f51de2a07c5d716b8738af76a268e878
Samuel Poirier <samuel.poirier@beslogic.com>
2026-08-25 09:45:16 -0400

    Add full neovim config remaining files

---
+++ omarchy/.config/nvim/lua/plugins/dotnet.lua
@@ -0,0 +1,23 @@
+return {
+  -- Mason: manages installed LSPs/tools
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry", -- has an up-to-date roslyn build
+      },
+    },
+  },
+
+  -- Debugger
+  {
+    "mfussenegger/nvim-dap",
+  },
+
+  -- Treesitter grammar for C#
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "c_sharp" } },
+  },
+}
