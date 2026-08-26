4a343b61e1e814e67e9736255bb730eb59d888f1
Samuel Poirier <samuel.poirier@beslogic.com>
2026-08-26 12:04:46 -0400

    Add and configure neotest-python

--- omarchy/.config/nvim/lua/plugins/test.lua
+++ omarchy/.config/nvim/lua/plugins/test.lua
@@ -1,18 +1,20 @@
 return {
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-neotest/nvim-nio",
-			"nsidorenco/neotest-vstest",
-			"nvim-neotest/neotest-go",
-		},
-		config = function()
-			require("neotest").setup({
-				adapters = {
-					require("neotest-vstest"),
-					require("neotest-go"),
-				},
-			})
-		end,
-	},
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nsidorenco/neotest-vstest",
+      "nvim-neotest/neotest-go",
+      "nvim-neotest/neotest-python",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-vstest"),
+          require("neotest-go"),
+          require("neotest-python"),
+        },
+      })
+    end,
+  },
 }
