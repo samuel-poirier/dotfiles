3ddd4f4b7063facb5c670d508f3435ce275f0b6f
Samuel Poirier <samuel.poirier@beslogic.com>
2026-08-29 11:01:38 -0400

    Cleanup lsp binding import

--- omarchy/.config/nvim/lua/config/keymaps.lua
+++ omarchy/.config/nvim/lua/config/keymaps.lua
@@ -119,8 +119,7 @@ map("n", "<C-A-m>", function()
   })
 end, opts)
 
-require("config/lsp-bindings/dotnet-bindings")
-require("config/lsp-bindings/neotest-bindings")
+require("config/lsp-bindings/lsp-bindings-init")
 
 -- Standard binding onlsp attach
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -135,17 +134,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     })
   end,
 })
-
----
-
--- { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition", has = "definition" },
--- { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
--- { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
--- { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
--- { "<leader>ss", function() Snacks.picker.lsp_symbols({ filter = LazyVim.config.kind_filter }) end, desc = "LSP Symbols", has = "documentSymbol" },
--- { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols({ filter = LazyVim.config.kind_filter }) end, desc = "LSP Workspace Symbols", has = "workspace/symbols" },
--- { "gai", function() Snacks.picker.lsp_incoming_calls() end, desc = "C[a]lls Incoming", has = "callHierarchy/incomingCalls" },
--- { "gao", function() Snacks.picker.lsp_outgoing_calls() end, desc = "C[a]lls Outgoing", has = "callHierarchy/outgoingCalls" },
 
 ---
 
---
+++ omarchy/.config/nvim/lua/config/lsp-bindings/lsp-bindings-init.lua
@@ -0,0 +1,2 @@
+require("config/lsp-bindings/dotnet-bindings")
+require("config/lsp-bindings/neotest-bindings")
